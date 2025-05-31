08fd42c22de9dbacc6c08ced7814f0b7d4e20804
Gilbert Marasigan <21050167+GilbertMarasigan@users.noreply.github.com>
2025-05-31 13:15:13 +0800

    13.4 GET POST DELETE blogs

--- index.js
+++ index.js
@@ -32,8 +32,11 @@ Note.init({
 }
 )
 
+Note.sync()
+
 app.get('/api/notes', async (req, res) => {
     const notes = await Note.findAll()
+    console.log(JSON.stringify(notes))
     res.json(notes)
 })
 
@@ -49,6 +52,27 @@ app.post('/api/notes', async (req, res) => {
         return res.status(400).json({ error })
     }
 
+})
+
+app.get('/api/notes/:id', async (req, res) => {
+    const note = await Note.findByPk(req.params.id)
+    if (note) {
+        console.log(note.toJSON())
+        res.json(note)
+    } else {
+        res.status(404).end()
+    }
+})
+
+app.put('/api/notes/:id', async (req, res) => {
+    const note = await Note.findByPk(req.params.id)
+    if (note) {
+        note.important = req.body.important
+        await note.save()
+        res.json(note)
+    } else {
+        res.status(404).end()
+    }
 })
 
 const PORT = process.env.PORT || 3001
